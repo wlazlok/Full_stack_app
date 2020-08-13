84e32b59ef9e037ba535254c84066871870a397e
unknown <kwlazo9@gmail.com>
2020-08-13 20:35:24 +0200

    added iamge controller to front images of each product on index page and upadate index page

--- src/main/java/karol/spring/webapp/controllers/ImageController.java
+++ src/main/java/karol/spring/webapp/controllers/ImageController.java
@@ -4,11 +4,11 @@ import karol.spring.webapp.models.Product;
 import karol.spring.webapp.services.ProductService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -84,4 +84,45 @@ public class ImageController {
             IOUtils.copy(is, response.getOutputStream());
         }
     }
+
+    @GetMapping("/frontImage/{id}")
+    public void getFrontImage(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        System.out.println("ZDJ: " + id);
+        if (productService.findProductById(id).getMainImage() == null)
+            System.out.println("Image not Found!");
+        else {
+            Product product = productService.findProductById(id);
+
+            byte[] byteArrys = new byte[product.getMainImage().length];
+
+            int i = 0;
+            for (Byte b : product.getMainImage()) {
+                byteArrys[i++] = b;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArrys);
+            IOUtils.copy(is, response.getOutputStream());
+        }
+    }
+
+//    @GetMapping("/indexImages")
+//    public void getIndexImages( HttpServletResponse response) throws IOException {
+//        List<Product> products = productService.getProducts();
+//
+//        System.out.println(products.size());
+//
+//        byte[] byteArrays = new byte[products.get(1).getMainImage().length];
+//        int i = 0;
+//        for (Byte b : products.get(1).getMainImage()) {
+//            byteArrays[i++] = b;
+//        }
+//
+//
+//        response.setContentType("image/jpeg");
+//        InputStream is = new ByteArrayInputStream(byteArrays);
+//        IOUtils.copy(is, response.getOutputStream());
+//
+//    }
+
 }
