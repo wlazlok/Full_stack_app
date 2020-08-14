609ab8ce17c2d7b05e8a2dc3b231f58412177b21
unknown <kwlazo9@gmail.com>
2020-08-14 17:02:42 +0200

    added possibility to create new company

--- src/main/java/karol/spring/webapp/controllers/CompanyController.java
+++ src/main/java/karol/spring/webapp/controllers/CompanyController.java
@@ -7,10 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/company")
@@ -52,6 +49,26 @@ public class CompanyController {
             savedCompany.setProducts(company.getProducts());
 
             companyService.saveCompany(company);
+
+            return "redirect:/company";
+        }
+    }
+
+    @GetMapping("/new")
+    public String createNewCompany(Model model){
+        model.addAttribute("company", new Company());
+
+        return "company/createNewCompanyForm";
+    }
+
+    @PostMapping("/new")
+    public String processCreateNewCompany(@ModelAttribute Company company, BindingResult result, Model model){
+        if(result.hasErrors()){
+            System.out.println("Problem during creating new company");
+            model.addAttribute("company", company);
+            return "redirect:/company/createNewCompanyForm";
+        }else{
+            Company savedCompany = companyService.saveCompany(company);
 
             return "redirect:/company";
         }
