5c54766e8317f7ba43fdf8336f0b431a52049fb5
unknown <kwlazo9@gmail.com>
2020-08-14 12:07:59 +0200

    added security to protect edit category by normal user

--- src/main/java/karol/spring/webapp/security/SecurityConfig.java
+++ src/main/java/karol/spring/webapp/security/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .and()
 //                .authorizeRequests().antMatchers("/category/*/delete").hasRole("ADMIN")
 //                .and()
+//                .authorizeRequests().antMatchers("/category/*/edit").hasRole("ADMIN")
+//                .and()
                 .headers().frameOptions().disable()
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**")
